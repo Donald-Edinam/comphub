c5e0b407c4810952d1282e5f02ec24cc48d2a381
Donald-Edianm <kobbyedinamdonald@gmail.com>
2025-08-20 13:15:36 +0000

    Add: Added dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Signin1 from "./pages/auth/Signup"
 import Login5 from "./pages/auth/Login"
+import Dashboard from "./pages/dashboard/Dashboard"
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/"  index element={<Home />} />
+        <Route path="/" index element={<Home />} />
         <Route path="/signup" element={<Signin1 />} />
         <Route path="/login" element={<Login5 />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   )
