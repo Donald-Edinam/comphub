f03673eac9efda79d2784640aee370e4f113ba6f
Donald-Edianm <kobbyedinamdonald@gmail.com>
2025-08-24 18:40:14 +0000

    Fix: Resolved cors error

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = 5000;
 const allowedOrigins = [
   "http://localhost:5173", // Vite default
   "http://localhost:5174", // Vite alt
-  "http://localhost:3000"  // React/Next.js
+ ""
 ];
 
 app.use(
